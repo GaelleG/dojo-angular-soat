48513ab6d766b9fba3cdbe8b77820736bd8ac151
Gaelle <pussy@pukogames.com>
2014-10-04 01:51:22 +0200

    Error display

--- app.js
+++ app.js
@@ -6,14 +6,22 @@ app.controller('MovieController', ['$scope', '$http', function ($scope, $http) {
 	$scope.id = "";
 	
 	$scope.response = {};
-	
+	$scope.error = "";
+
 	$scope.getMovie = function () {
 		$http.get('http://www.omdbapi.com/?t=' + $scope.title + '&y=' + $scope.year + '&i=' + $scope.id)
 		.success(function (data) {
-			$scope.response = data;
+			if (data.Response == "False") {
+				$scope.error = data.Error;
+				$scope.response = {};
+			}
+			else {
+				$scope.error = "";
+				$scope.response = data;
+			}
 		})
-		.error(function (data) {
-			$scope.response = data;
+		.error(function (data, status) {
+			$scope.error = "Une erreur est survenue";
 		});
 	};
 }]);
