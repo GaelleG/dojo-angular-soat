0f0c4f3bc9209ce149e1b99e5e369b763b06bf41
Gaelle <pussy@pukogames.com>
2014-10-04 01:21:56 +0200

    $http: OMDb API request

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 var app = angular.module('dojo', []);
 
-app.controller('MovieController', ['$scope', function ($scope) {
+app.controller('MovieController', ['$scope', '$http', function ($scope, $http) {
 	$scope.title = "";
 	$scope.year = "";
 	$scope.id = "";
+	
+	$scope.response = {};
+	
+	$scope.getMovie = function () {
+		$http.get('http://www.omdbapi.com/?t=' + $scope.title + '&y=' + $scope.year + '&i=' + $scope.id)
+		.success(function (data) {
+			$scope.response = data;
+		})
+		.error(function (data) {
+			$scope.response = data;
+		});
+	};
 }]);
