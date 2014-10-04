e2390026a36b9766ca437ede2a00c8c7d7284beb
Gaelle <pussy@pukogames.com>
2014-10-04 02:33:03 +0200

    Movie display

--- app.js
+++ app.js
@@ -5,19 +5,22 @@ app.controller('MovieController', ['$scope', '$http', function ($scope, $http) {
 	$scope.year = "";
 	$scope.id = "";
 	
-	$scope.response = {};
+	$scope.response = null;
 	$scope.error = "";
+	$scope.movieKeys = [];
 
 	$scope.getMovie = function () {
 		$http.get('http://www.omdbapi.com/?t=' + $scope.title + '&y=' + $scope.year + '&i=' + $scope.id)
 		.success(function (data) {
 			if (data.Response == "False") {
 				$scope.error = data.Error;
-				$scope.response = {};
+				$scope.response = null;
+				$scope.movieKeys = [];
 			}
 			else {
 				$scope.error = "";
 				$scope.response = data;
+				$scope.movieKeys = Object.keys(data);
 			}
 		})
 		.error(function (data, status) {
