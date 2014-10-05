fe2e2fd34232816ed58752f6fe20f08b9b1c9efd
Gaelle <pussy@pukogames.com>
2014-10-05 21:22:08 +0200

    $watch: single search box

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ app.controller('MovieController', ['$scope', '$http', function ($scope, $http) {
 	$scope.title = "";
 	$scope.year = "";
 	$scope.id = "";
+	$scope.meltedProperties = "";
 	
 	$scope.response = null;
 	$scope.error = "";
@@ -34,6 +35,74 @@ app.controller('MovieController', ['$scope', '$http', function ($scope, $http) {
 			$scope.error = "";
 		}
 	};
+
+	$scope.$watch('meltedProperties', function() {
+		if ($scope.meltedProperties.length == 0) {
+			$scope.title = "";
+			$scope.year = "";
+			$scope.id = "";
+			$scope.error = "";
+			return;
+		}
+
+		var melted = $scope.meltedProperties;
+
+		var strRegexIMDbID = "tt(\\d+)";
+		var regexIMDbID = new RegExp(strRegexIMDbID);
+		var strRegexYear = "(19|20)(\\d){2}";
+		var regexYear = new RegExp(strRegexYear);
+
+		var regexIMDbIDWithSeparator = new RegExp(
+			"(^" + strRegexIMDbID + "$)" + "|" +
+			"(((\\W|\\s)+)" + strRegexIMDbID + "$)" + "|" +
+			"(^" + strRegexIMDbID + "(((\\W|\\s)+)))" + "|" +
+			"(^" + strRegexYear + "((\\W|\\s)+)" + strRegexIMDbID + "((\\W|\\s)+)(\\D+)$)" + "|" +
+			"(^(\\D+)((\\W|\\s)+)" + strRegexIMDbID + "((\\W|\\s)+)" + strRegexYear + "$)");
+		var regexYearWithSeparator = new RegExp(
+			"(^" + strRegexYear + "((\\W|\\s)+))" + "|" +
+			"(((\\W|\\s)+)" + strRegexYear + "$)");
+
+		if (regexIMDbIDWithSeparator.test(melted)) {
+			$scope.id = melted.match(regexIMDbID)[0];
+			melted = melted.replace($scope.id, "");
+			while ((/^(\W|\s)+/).test(melted)) {
+				melted = melted.substring(1, melted.length);
+			}
+			while ((/(\W|\s)+$/).test(melted)) {
+				melted = melted.substring(0, melted.length-1);
+			}
+		}
+		else {
+			$scope.id = "";
+		}
+
+		if (regexYearWithSeparator.test(melted)) {
+			var s = melted;
+			while (regexYearWithSeparator.test(s)) {
+				$scope.year = s.match(regexYear)[0];
+				s = s.replace($scope.year, "");
+			}
+			melted = melted.replace($scope.year, "");
+			while ((/^(\W|\s)+/).test(melted)) {
+				melted = melted.substring(1, melted.length);
+			}
+			while ((/(\W|\s)+$/).test(melted)) {
+				melted = melted.substring(0, melted.length-1);
+			}
+		}
+		else {
+			$scope.year = "";
+		}
+
+		if (melted.length > 0) {
+			$scope.title = melted;
+		}
+		else {
+			$scope.title = "";
+		}
+
+		$scope.getMovie();
+	});
 
 	$scope.getAPIResponse = function (urlParams) {
 		if (urlParams === undefined) return;
